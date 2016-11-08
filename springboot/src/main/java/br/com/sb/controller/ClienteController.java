dfb49dc9e042dd6f80777a8c67ddb6a59e02411b
Gustavo Viais de Brito Silva <gustavoviais@gmail.com>
2016-11-08 10:57:29 -0200

    Inicio Projeto

---
+++ springboot/src/main/java/br/com/sb/controller/ClienteController.java
@@ -0,0 +1,13 @@
+package br.com.sb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClienteController {
+	@RequestMapping(method=RequestMethod.GET, value="/clientes")
+	public void buscar(){
+		System.out.println("Chamou /clientes...");
+	}
+}
