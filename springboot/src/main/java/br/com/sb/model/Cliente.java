0b6fc0974585ab4a455ee6d3919b76b640dad85f
Gustavo Viais de Brito Silva <gustavoviais@gmail.com>
2016-11-12 09:28:01 -0200

    Aula 05

--- springboot/src/main/java/br/com/sb/model/Cliente.java
+++ springboot/src/main/java/br/com/sb/model/Cliente.java
@@ -1,6 +1,13 @@
 package br.com.sb.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private String nome;
 	
